d67b6e7e96593172ed9dcb7eed64662f832b4094
pustian-rafiq <rafiqul.pust.cse@gmail.com>
2022-06-27 10:39:43 +0600

    design signup form

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,21 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+  <SignUpVue/>
 </template>
 
 <script>
- 
+import SignUpVue from './components/SignUp.vue'
+
 export default {
   name: 'App',
+  components: {
+    SignUpVue,
+  }
+
  
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+ 
 </style>
---
+++ src/components/SignUp.vue
@@ -0,0 +1,60 @@
+<template>
+
+    <div class="container mt-5">
+        <div class="row">
+            <div class="col-sm-12 col-md-6 offset-md-3">
+                <div class="card" style=" margin: auto;">
+                    <div class="card-header bg-dark text-light text-center">
+                        Signup Form
+                    </div>
+                    <div class="card-body">
+                        <form action="">
+                            <div class="row">
+                                <div class="col-sm-12 col-md-12 mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Name</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="Enter your name">
+                                </div>
+                                <div class="col-sm-12 col-md-12 mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Email</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="Enter your email">
+                                </div>
+                                <div class="col-sm-12 col-md-12 mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Password</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="Enter your password">
+                                </div>
+                                <div class="col-sm-12 col-md-12 mb-3">
+                                    <label for="exampleFormControlInput1" class="form-label">Confirm Password</label>
+                                    <input type="email" class="form-control" id="exampleFormControlInput1"
+                                        placeholder="Enter your confirm password">
+                                </div>
+
+                                <div class="col-md-12 mb-3">
+                                    <button class="btn btn-success">Signup</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+
+        </div>
+    </div>
+ 
+
+
+</template>
+
+<script>
+
+export default {
+    name: "SignUp"
+}
+
+</script>
+
+<style scoped>
+</style>
